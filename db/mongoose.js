7ea141917f5b30a15f549057a54f1c83dffa96bc
DavidWei95036593 <dw36593@pausd.us>
2018-05-15 09:20:53 -0700

    make mongoose correct, and make a register route for login

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+//||'mongodb://localhost:27017/assignmentLink'
 const database = process.env.MONGODB_URI ||
-                 'mongodb://localhost:27017/assignmentLink';
+                 'mongodb://localhost:27017/assignmentlink';
 mongoose.connect(database)
   .then(() => {
     console.log(`Database up on ${database}`);
