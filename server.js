d152190800164ebd8d539ec6e715999402e7505e
Jeremy <jeremyxu123@gmail.com>
2018-05-18 13:19:40 -0700

    get css to work

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set(`view engine`,`hbs`);
     cookie:{secure:false}
   }));
 
+  app.use(express.static(path.join(__dirname,`public`)));
   app.use(flash());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
