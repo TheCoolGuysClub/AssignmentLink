e6d9919f993ea0996c920c76f1ad8e8a7b4629c7
Jeremy <jeremyxu123@gmail.com>
2018-05-25 13:15:57 -0700

    fix again for jeremys heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //'mongodb://localhost:27017/assignmentLink'
-const database = process.env.MONGOLAB_URI ||
+const database = process.env.MONGODB_URI ||
                  'mongodb://localhost:27017/assignmentlink';
 mongoose.connect(database)
   .then(() => {
